8bd220453239f7104faa028de97fa8b0e14cadcc
GraceFC <huangld0314@gmail.com>
2021-02-10 11:35:20 -0800

    Add code of getting random message.

--- messageGenerator.js
+++ messageGenerator.js
@@ -8,3 +8,25 @@ const messagePool = {
     food: ['eggs', 'sandwich', 'avocado', 'lynchee', 'apples', 'crabs'],
     exercising: ['running', 'pilates', 'playing tennis', 'swimming']
 }
+
+const randomMessage = [];
+
+for (let item in messagePool) {
+    itemLength = messagePool[item].length;
+    messageIDX = random (itemLength);
+    switch (item) {
+        case 'color':
+            return randomMessage.push(`This customer's favorite color is: ${messagePool[item][messageIDX]}`);
+            break;
+        case 'food':
+            return randomMessage.push(`This customer's favorite food is: ${messagePool[item][messageIDX]}`);
+            break;
+        case 'exercising':
+            return randomMessage.push(`This customer's favorite exercise is: ${messagePool[item][messageIDX]}`);
+            break;
+        default:
+            return "The customer didn't provide correct information."
+            break;
+    }
+
+}
