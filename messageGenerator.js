421efc3217eecc598ee1bc78c046d53fc833b40c
GraceFC <huangld0314@gmail.com>
2021-02-19 23:31:29 -0800

    Correct running results.

--- messageGenerator.js
+++ messageGenerator.js
@@ -3,30 +3,58 @@ const randomNum = num => {
     return Math.floor(Math.random() * num);
 }
 
-const messagePool = {
-    color: ['white', 'red', 'yellow', 'blue', 'orange', 'green'],
-    food: ['eggs', 'sandwich', 'avocado', 'lynchee', 'apples', 'crabs'],
-    exercising: ['running', 'pilates', 'playing tennis', 'swimming']
-}
 
-const randomMessage = [];
 
-for (let item in messagePool) {
-    itemLength = messagePool[item].length;
-    messageIDX = random (itemLength);
-    switch (item) {
-        case 'color':
-            return randomMessage.push(`This customer's favorite color is: ${messagePool[item][messageIDX]}`);
-            break;
-        case 'food':
-            return randomMessage.push(`This customer's favorite food is: ${messagePool[item][messageIDX]}`);
-            break;
-        case 'exercising':
-            return randomMessage.push(`This customer's favorite exercise is: ${messagePool[item][messageIDX]}`);
-            break;
-        default:
-            return "The customer didn't provide correct information."
-            break;
+
+
+const printMessage = () => {
+    const messagePool = {
+        color: ['white', 'red', 'yellow', 'blue', 'orange', 'green'],
+        food: ['eggs', 'sandwich', 'avocado', 'lynchee', 'apples', 'crabs'],
+        exercising: ['running', 'pilates', 'playing tennis', 'swimming']
+    }
+    const randomMessage = [];
+    itemIDX = randomNum (3);
+    if (itemIDX == 0) {
+        let messageIDX = randomNum (messagePool.color.length);
+        randomMessage.push(`This customer's favorite color is: ${messagePool.color[messageIDX]}`);
+        return randomMessage;
+    } else if (itemIDX == 1) {
+        let messageIDX = randomNum (messagePool.food.length);
+        randomMessage.push(`This customer's favorite food is: ${messagePool.food[messageIDX]}`);
+        return randomMessage;
+    } else if (itemIDX == 2) {
+        let messageIDX = randomNum (messagePool.exercising.length);
+        randomMessage.push(`This customer's favorite excercising: ${messagePool.exercising[messageIDX]}`);
+        return randomMessage;
+    } else {
+        return "The customer didn't provide correct information.";
     }
 
+
+    /*for (let item in messagePool) {
+        let itemLength = messagePool[item].length;
+        let messageIDX = randomNum (itemLength);
+        switch (item) {
+            case 'color':
+                randomMessage.push(`This customer's favorite color is: ${messagePool[item][messageIDX]}`);
+                return randomMessage;
+                break;
+            case 'food':
+                randomMessage.push(`This customer's favorite food is: ${messagePool[item][messageIDX]}`);
+                return randomMessage;
+                break;
+            case 'exercising':
+                randomMessage.push(`This customer's favorite exercise is: ${messagePool[item][messageIDX]}`);
+                return randomMessage;
+                break;
+            default:
+                return "The customer didn't provide correct information.";
+                break;
+        }
+    } */
 }
+
+
+
+console.log(printMessage());
